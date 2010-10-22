e491a9988282cd12b2737b213318fe9e6535a334
Eliot Shepard <eshepard@slower.net>
2010-10-22 17:05:28 -0400

    Working venue import. Started bond create.

--- oots.rb
+++ oots.rb
@@ -46,15 +46,19 @@ Dir[File.join(File.dirname(__FILE__), "app", "controllers", "*.rb")].each { |fil
 
 #####################
 
+def teams_for_select
+	Team.all(:order => 'name').map { |t| { 
+		:id => t.id, 
+		:label => t.name, 
+		:altnames => t.altnames, 
+		:definite_article => t.definite_article
+	}}
+end
+
 get '/' do
 	# @teams = CACHE.fetch("teams") do
-		@teams = Team.all(:order => 'name').map { |t| { 
-			:id => t.id, 
-			:label => t.name, 
-			:altnames => t.altnames, 
-			:definite_article => t.definite_article
-		}}
 	# end
+	@teams = teams_for_select
 	erb :'venues/search'
 end
 
@@ -68,9 +72,34 @@ get '/venues/search' do
 	respond_to do |wants|
 		wants.html { redirect '/' }
 		wants.js {
+			if params[:foursquare_id]
+				Venue.where(:foursquare_id => params[:foursquare_id].to_i).all.to_json
+			end
 			# Venue.where().sort($near : [50,50])
 		}
 	end
+end
+
+get '/venues/:slug' do
+	@venue = Venue.find_by_slug(params[:slug])
+	@teams = teams_for_select
+	erb :'venues/show'
+end
+
+get '/venues/:slug/edit' do
+	@venue = Venue.find_by_slug(params[:slug])
+	@form = { :method => 'put', :endpoint => '/venues' }
+	erb :'teams/edit'
+end
+
+get '/venues/f/:foursquare_id' do
+	venue = Venue.where(:foursquare_id => params[:foursquare_id].to_i).all.first
+	redirect venue.permalink
+end
+
+post '/venues' do
+	venue = Venue.create!((params[:venue]))
+	redirect venue.permalink
 end
 
 get '/teams/new' do
--- public/javascripts/public.js
+++ public/javascripts/public.js
@@ -1,6 +1,12 @@
 $(document).ready(function() {
-	_.templateSettings = { interpolate : /\#\{(.+?)\}/g };
+	_.templateSettings = { interpolate : /\#\{(.+?)\}/g };	// #{} style
 
+	// venue search /////////////////////////////////////////
+
+	if ((c = $.cookies.get('search-city')) && c.lat) {
+		$('#search-city').html(c.city).addClass('preliminary').closest('#search-form').data('city', c.city).data('lat', c.lat).data('lon', c.lon);
+	}
+	
 	$('#search-team').focus().autocomplete({ 
 		source: function(r, cb) {
 			var teams = _.select($('#search-team').data('teams'), function(team) {
@@ -12,37 +18,49 @@ $(document).ready(function() {
 			$(this).blur().closest('#search-form').data('team_id', ui.item.id).data('team_name', ui.item.label);
 			ui.item.definite_article ? $('#search-team-definite-article').show() : $('#search-team-definite-article').hide();
 			$.getJSON('/venues/search.js?' + $.param($(this).closest('#search-form').data()) + '&callback=?', function() {
-				
 			});
 		}
 	});
 	
-	if ((c = $.cookies.get('search-city')) && c.lat) {
-		$('#search-city').html(c.city).addClass('preliminary').closest('#search-form').data('city', c.city).data('lat', c.lat).data('lon', c.lon);
-	}
-	
 	if ($('#search-city').length && navigator.geolocation) {
 		navigator.geolocation.getCurrentPosition(function(position) {
 			var c = position.coords;
-			$.yql("select * from geo.placefinder where text='" + c.latitude + " " + c.longitude + "' and gflags='R'", 
-				function(data) {
-					var r = data.query.results['Result'];
+			$.ajax({
+				url: "http://query.yahooapis.com/v1/public/yql",
+				dataType: "jsonp",
+				data: {
+					q: "select * from geo.placefinder where text='" + c.latitude + " " + c.longitude + "' and gflags='R'",
+					format: 'json',
+					callback: '?'
+				},
+				success: function(d) {
+					var r = d.query.results['Result'];
 					$.cookies.set('search-city', { city: r.city, lat: c.latitude, lon: c.longitude });
 					$('#search-city').html(r.city).removeClass('preliminary').closest('#search-form').data('city', r.city).data('lat', c.latitude).data('lon', c.longitude);
-				}
-			);
+				}				
+			});
 		}, function(m) {
 			console.log(m);
 		});
 	}
 
+	// venue edit /////////////////////////////////////////
+	
 	$('#venue-search-location').change(function() {
 		var $that = $(this);
-		$.yql("select * from geo.placefinder where text='" +$(this).val() + "' and gflags=''", function(d) {
-			var r = d.query.results['Result'];
-			$that.closest('.form').data('lat', r.latitude).data('lon', r.longitude);
+		$.ajax({
+			url: "http://query.yahooapis.com/v1/public/yql",
+			dataType: "jsonp",
+			data: {
+				q: "select * from geo.placefinder where text='" + $(this).val() + "'",
+				format: 'json',
+				callback: '?'
+			},
+			success: function(d) {
+				var r = d.query.results['Result'];
+				$that.closest('.form').data('lat', r.latitude).data('lon', r.longitude);
+			}
 		});
-		
 	});
 	
 	$('#venue-search-name').autocomplete({
@@ -57,8 +75,13 @@ $(document).ready(function() {
 					callback: '?'
 				},
 				success: function(d) {
-					var venues = d.query.results.venues.group.venue; venues = _.isArray(venues) ? venues : Array(venues);
-					venues = _.map(venues, function(v) { return { label: v.name + ": " + v.address, id: v.id } });
+					// massage inconsistently structured 4sq API results
+					var venues = d.query.results.venues;
+					venues = _.isArray(venues.group) ? venues.group : Array(venues.group);
+					venues = _.select(venues, function(g) { return(g.type == 'Matching Places'); } ).shift().venue; 
+					venues = _.isArray(venues) ? venues : Array(venues);
+					$('#venue-search-name').data('venues', venues);
+					venues = _.map(venues, function(v) { return { label: v.name + " @ " + v.address, id: v.id, distance: parseInt(v.distance) } }).sort(function(a,b) { return((a.distance < b.distance) ? -1 : 1); });
 					cb(venues); 
 				}
 			});
@@ -66,22 +89,33 @@ $(document).ready(function() {
 		delay: 500,
 		minLength: 2,
 		select: function(e, ui) {
-			$(this).blur().closest('#search-form').data('team_id', ui.item.id).data('team_name', ui.item.label);
-			ui.item.definite_article ? $('#search-team-definite-article').show() : $('#search-team-definite-article').hide();
-			$.getJSON('/venues/search.js?' + $.param($(this).closest('#search-form').data()) + '&callback=?', function() {
-				
+			var venue = _.select($('#venue-search-name').data('venues'), function(v) { return (v.id == ui.item.id); }).shift();
+
+			// check whether we have this venue already
+			$.getJSON('/venues/search.js', { foursquare_id: venue.id }, function(d) {
+				if (d.length > 0) { document.location.href = '/venues/f/' + venue.id; }
+			});
+			
+			$('#venue-pre-edit-search').hide();
+			$('#venue-edit').fadeIn();
+			
+			$('#venue_name').val(venue['name']);
+			$.each(['address', 'city', 'state', 'zip', 'crossstreet', 'phone', 'twitter', 'foursquare_id'], function(i, v) {
+				jQuery('#venue_' + v).val(venue[v]);
 			});
 		}
 	});
 
-	// $('#venue-search-name').change(function() {
-	// 	var $that = $(this);
-	// 	$.yql("use 'http://www.datatables.org/foursquare/foursquare.venues.xml' as foursquare.venues; select * from foursquare.venues where q='" + $that.val() + "' and geolat='" + $that.closest('.form').data('lat') + "' and geolong='" + $that.closest('.form').data('lon') + "'", function(d) {
-	// 		var venues = d.query.results.venues.group.venue; venues = _.isArray(venues) ? venues : Array(venues);
-	// 		_.each(venues, function(v) {
-	// 			console.log(v);
-	// 			$('#venue-search-results').prepend($.trim(_.template($('.template#search-venue').html(), v)));
-	// 		});
-	// 	});			
-	// });
+	// bond edit /////////////////////////////////////////
+
+	$('#bond-team-name').autocomplete({
+		source: function(r, cb) {
+			var teams = _.select($('#bond-team-name').data('teams'), function(team) {
+				return (team.label + " " + (team.altnames == null ? '' : team.altnames)).match(new RegExp(r.term, 'i')) != null; 
+			});
+			cb(teams);
+		},
+		select: function(e, ui) {
+		}		
+	});
 });
